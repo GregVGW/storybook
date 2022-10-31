ef4bfbc2546e41d47f2cf587d273a96ddaf2e8f5
GregVGW <greg.lahaye@vgw.co>
2022-10-31 19:09:03 +0800

    add html-vite framework

--- code/lib/cli/src/versions.ts
+++ code/lib/cli/src/versions.ts
@@ -41,6 +41,7 @@ export default {
   '@storybook/docs-tools': '7.0.0-alpha.46',
   '@storybook/ember': '7.0.0-alpha.46',
   '@storybook/html': '7.0.0-alpha.46',
+  '@storybook/html-vite': '7.0.0-alpha.46',
   '@storybook/html-webpack5': '7.0.0-alpha.46',
   '@storybook/instrumenter': '7.0.0-alpha.46',
   '@storybook/nextjs': '7.0.0-alpha.46',
